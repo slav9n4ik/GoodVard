f263646a9163345fc98d4d2bbb69b3a8ce195d14
slav9n4ik <slava.tresnevv@gmail.com>
2019-08-04 21:05:23 +0300

    refactoring discount

--- goodvard/src/components/content/discount/Discount.js
+++ goodvard/src/components/content/discount/Discount.js
@@ -2,17 +2,12 @@ import React, { Component, Fragment } from "react";
 import "./discount.css";
 
 import discountImg1 from "../../../img/discount1.png";
-import discountImg2 from "../../../img/discount3.png";
+import discountImg2 from "../../../img/discount2.png";
 import discountImg3 from "../../../img/discount3.png";
 
 class Discount extends Component {
   render() {
-    const options = {
-      items: 1,
-      nav: true,
-      rewind: true,
-      autoplay: true
-  };
+
     return (
       <Fragment>
         <div id="discount" className="container-fluid">
@@ -25,17 +20,38 @@ class Discount extends Component {
             <div className="col-xl-7 col-12">
               <div className="container-fluid discount-container">
                 <div className="row justify-content-center">
-                  {/* <div className="col-sm-4">
+                  <div className="col-sm-4 d-none d-md-block">
                     <img src={discountImg1} alt=''/>
                   </div>
-                  <div className="col-sm-4">
+                  <div className="col-sm-4 d-none d-md-block">
                     <img src={discountImg2} alt=''/>
                   </div>
-                  <div className="col-sm-4">
+                  <div className="col-sm-4 d-none d-md-block">
                     <img src={discountImg3} alt=''/>
-                  </div> */}
-                  
-                  
+                  </div>
+
+                  <div id="carouselExampleControls" class="carousel slide d-md-none" data-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src={discountImg1} class="d-block w-100" alt="..."/>
+                      </div>
+                      <div class="carousel-item">
+                        <img src={discountImg2} class="d-block w-100" alt="..."/>
+                      </div>
+                      <div class="carousel-item">
+                        <img src={discountImg3} class="d-block w-100" alt="..."/>
+                      </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+
                 </div>
               </div>
             </div>
