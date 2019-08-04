4d546f448d7f11de762869b5cbbae6334e371462
slav9n4ik <slava.tresnevv@gmail.com>
2019-08-04 19:34:28 +0300

    add

--- goodvard/src/components/content/discount/Discount.js
+++ goodvard/src/components/content/discount/Discount.js
@@ -1,12 +1,6 @@
 import React, { Component, Fragment } from "react";
 import "./discount.css";
 
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
-import jquery from 'jquery';
-import $ from 'jquery';
-
 import discountImg1 from "../../../img/discount1.png";
 import discountImg2 from "../../../img/discount3.png";
 import discountImg3 from "../../../img/discount3.png";
@@ -40,25 +34,7 @@ class Discount extends Component {
                   <div className="col-sm-4">
                     <img src={discountImg3} alt=''/>
                   </div> */}
-                  <OwlCarousel
-                      className="owl-theme"
-                      loop
-                      margin={10}
-                      nav
-                  >
-                      <div class="item"><h4>1</h4></div>
-                      <div class="item"><h4>2</h4></div>
-                      <div class="item"><h4>3</h4></div>
-                      <div class="item"><h4>4</h4></div>
-                      <div class="item"><h4>5</h4></div>
-                      <div class="item"><h4>6</h4></div>
-                      <div class="item"><h4>7</h4></div>
-                      <div class="item"><h4>8</h4></div>
-                      <div class="item"><h4>9</h4></div>
-                      <div class="item"><h4>10</h4></div>
-                      <div class="item"><h4>11</h4></div>
-                      <div class="item"><h4>12</h4></div>
-                  </OwlCarousel>
+                  
                   
                 </div>
               </div>
